4ec84fb5ed5c00abb24a7f0635cb8562f0748f65
ilia shokry <iliashoky2022@gmail.com>
2025-12-15 16:22:06 +0330

    dididid

---
+++ app/Models/Event.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\belongsTo;
+
+class Event extends Model
+{
+    use HasFactory;
+
+
+
+    public function  user() : belongsTo
+    {
+
+        return $this->belongsTo(User::class);
+    }
+    public function attendees():hasMany {
+
+        return $this->hasMany(Attendee::class);
+    }
+}
